d6ec50779a0ec240ed59f87540291ba8d2f834e1
arman mahmoudbeik <arman@gmail.com>
2018-06-24 00:18:42 +0430

    add bye method in controllers class

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,6 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use function Sodium\crypto_box_publickey_from_secretkey;
 
 class Controller extends BaseController
 {
@@ -17,5 +16,9 @@ class Controller extends BaseController
     {
         return "hello world";
     }
-}
 
+    public function bye()
+    {
+        return "bye world";
+    }
+}
